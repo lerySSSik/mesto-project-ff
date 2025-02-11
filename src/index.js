aa39ec0fb76507d71f9e56735d6fe89ec38e77b0
Valeria Kulagina <v_valeryevna@mail.ru>
2025-02-11 16:51:01 +0400

    create file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('Hello, World!');
+import './styles/index.css'; 
+// теперь картинки можно импортировать,
+// вебпак добавит в переменные правильные пути
+import jordanImage from './images/jordan.jpg';
+import jamesImage from './images/james.jpg';
+import bryantImage from './images/bryant.jpg';
+
+const whoIsTheGoat = [
+  // меняем исходные пути на переменные
+  { name: 'Michael Jordan', link: jordanImage },
+  { name: 'Lebron James', link: jamesImage },
+  { name: 'Kobe Bryant', link: bryantImage },
+]; 
+
+
+// ВТОРОЙ СПОСОБ РАБОТЫ С ИЗОБРАЖЕНИЯМИ(работает без запуска ВЕбпака)
+// // теперь картинки можно импортировать,
+// // вебпак добавит в переменные правильные пути
+// const jordanImage = new URL('./images/jordan.jpg', import.meta.url);
+// const jamesImage = new URL('./images/james.jpg', import.meta.url);
+// const bryantImage = new URL('./images/bryant.jpg', import.meta.url)
+
+// const whoIsTheGoat = [
+//   // меняем исходные пути на переменные
+//   { name: 'Michael Jordan', link: jordanImage },
+//   { name: 'Lebron James', link: jamesImage },
+//   { name: 'Kobe Bryant', link: bryantImage },
+// ]; 
+
